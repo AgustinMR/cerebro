ceab9b4e1b7188936a8a18ee202b429eb4dce05c
bruno-full-name <bruno.carballido@gmail.com>
2017-05-03 15:32:18 -0300

    cambios dalUsuarios 2/2

---
+++ MainCagado/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using cerebro_DataAccessLayer;
+
+namespace MainCagado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DALUsuario dalUsu = new DALUsuario();
+            if (dalUsu.agregarVisitante("bruno.carballido@gmail.com", "Bruno", "Mdeo2") == true)
+            {
+                Console.WriteLine("YEEEES");
+                Console.ReadLine();
+            }
+            else {
+                Console.WriteLine(":(");
+                Console.ReadLine();
+            }
+        }
+    }
+}
