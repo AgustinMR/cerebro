4db11212bd31519cf6af49aa55fcf4f42b17200f
bruno-full-name <bruno.carballido@gmail.com>
2017-05-03 15:15:00 -0300

    postgresql

---
+++ cerebro/Administrador.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cerebro
+{
+    public class Administrador : Usuario
+    {
+        public String password { get; set; }
+
+        public Administrador() { }
+        public Administrador(String email, String nombre_muni, String nombre, String password)
+        {
+            this.email = email;
+            this.nombre_muni = nombre_muni;
+            this.nombre = nombre;
+            this.password = password;
+        }
+    }
+}
