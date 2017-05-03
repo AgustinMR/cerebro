f6d8456c137b80c6ca9c28bef80611e948f1a1c3
bruno-full-name <bruno.carballido@gmail.com>
2017-05-03 15:27:50 -0300

    cambios dalUsuario 1/2

---
+++ cerebro/Operador.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cerebro
+{
+    public class Operador : Usuario
+    {
+        public String password { get; set; }
+
+        public Operador() { }
+        public Operador(String email, String nombre_muni, String nombre, String password)
+        {
+            this.email = email;
+            this.nombre_muni = nombre_muni;
+            this.nombre = nombre;
+            this.password = password;
+        }
+    }
+}
---
+++ cerebro/Visitante.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cerebro
+{
+    public class Visitante : Usuario
+    {
+        public Visitante() { }
+        public Visitante(String email, String nombre_muni, String nombre)
+        {
+            this.email = email;
+            this.nombre_muni = nombre_muni;
+            this.nombre = nombre;
+        }
+    }
+}
