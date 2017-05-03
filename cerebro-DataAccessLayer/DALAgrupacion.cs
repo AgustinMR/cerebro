b2023e78c2d09c8304a4d31be411ad29185a1a3c
bruno-full-name <bruno.carballido@gmail.com>
2017-05-03 18:01:47 -0300

    DALUsuario DALminicipalidad y agrupaciones

--- cerebro-DataAccessLayer/DALAgrupacion.cs
+++ cerebro-DataAccessLayer/DALAgrupacion.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using cerebro;
+using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,71 @@ using System.Threading.Tasks;
 
 namespace cerebro_DataAccessLayer
 {
-    public class DALAgrupacion
+    public class DALAgrupacion : IDALAgrupacion
     {
+        private NpgsqlConnection iniciarConeccion()
+        {
+            NpgsqlConnection conn = new NpgsqlConnection("Server=127.0.0.1;Port=5432;Database=cerebroDB;User Id=postgres;Password=postgres;");
+            conn.Open();
+            if (conn.State == System.Data.ConnectionState.Open)
+                return conn;
+            return null;
+        }
+
+        public bool agregarAgrupacion(Agrupacion grupo, Usuario usu)
+        {
+
+            NpgsqlConnection conn = iniciarConeccion();
+            NpgsqlCommand command = new NpgsqlCommand("INSERT INTO agrupaciones (nombre, nombre_municipalidad) VALUES ('" + grupo.nombre + "','" + grupo.nombre_muni + "')", conn);
+            command.ExecuteNonQuery();
+            NpgsqlCommand command2 = new NpgsqlCommand("INSERT INTO agrupaciones_usuarios (nombre_agrupacion, nombre_municipalidad_agrupacion, usuario_email, nombre_municipalidad_usuario, admin) VALUES ('" + grupo.nombre + "','" + grupo.nombre_muni + "','" + usu.email + "','" + usu.nombre_muni + "',true)", conn);
+            command2.ExecuteNonQuery();
+            conn.Close();
+            return true;
+        }
+
+        public bool agregarUsuariosAgrupacion(Agrupacion grupo, Usuario usu)
+        {
+
+            NpgsqlConnection conn = iniciarConeccion();
+            NpgsqlCommand command = new NpgsqlCommand("INSERT INTO agrupaciones_usuarios (nombre_agrupacion, nombre_municipalidad_agrupacion, usuario_email, nombre_municipalidad_usuario, admin) VALUES ('" + grupo.nombre + "','" + grupo.nombre_muni + "','" + usu.email + "','" + usu.nombre_muni + "',false)", conn);
+            command.ExecuteNonQuery();
+            conn.Close();
+            return true;
+        }
+
+        public bool cambiarAdminAgrupacion(Agrupacion grupo, Usuario usu, bool esAdmin)
+        {
+
+            NpgsqlConnection conn = iniciarConeccion();
+            NpgsqlCommand command = new NpgsqlCommand("UPDATE agrupaciones_usuarios SET admin="+esAdmin.ToString()+" WHERE nombre_agrupacion='" + grupo.nombre + "' AND nombre_municipalidad_agrupacion='" + grupo.nombre_muni + "' AND usuario_email='" + usu.email + "' AND nombre_municipalidad_usuario='" + usu.nombre_muni + "'", conn);
+            command.ExecuteNonQuery();
+            conn.Close();
+            return true;
+        }
+
+        public bool borrarUsuarioAgrupacion(Agrupacion grupo, Usuario usu)
+        {
+
+            NpgsqlConnection conn = iniciarConeccion();
+            NpgsqlCommand command = new NpgsqlCommand("DELETE FROM agrupaciones_usuarios WHERE nombre_agrupacion='" + grupo.nombre + "' AND nombre_municipalidad_agrupacion='" + grupo.nombre_muni + "' AND usuario_email='" + usu.email + "' AND nombre_municipalidad_usuario='" + usu.nombre_muni + "'", conn);
+            command.ExecuteNonQuery();
+            conn.Close();
+            return true;
+        }
+
+        public bool borrarAgrupacion(Agrupacion grupo)
+        {
+
+            NpgsqlConnection conn = iniciarConeccion();
+            NpgsqlCommand command = new NpgsqlCommand("DELETE FROM agrupaciones_usuarios WHERE nombre_agrupacion='" + grupo.nombre + "' AND nombre_municipalidad_agrupacion='" + grupo.nombre_muni + "'", conn);
+            command.ExecuteNonQuery();
+            NpgsqlCommand command2 = new NpgsqlCommand("DELETE FROM agrupaciones WHERE nombre='" + grupo.nombre + "' AND nombre_municipalidad='" + grupo.nombre_muni + "'", conn);
+            command2.ExecuteNonQuery();
+            conn.Close();
+            return true;
+        }
+
+
     }
 }
--- cerebro-DataAccessLayer/DALMunicipalidad.cs
+++ cerebro-DataAccessLayer/DALMunicipalidad.cs
@@ -1,12 +1,50 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using cerebro;
 
 namespace cerebro_DataAccessLayer
 {
-    class DALMunicipalidad
+    public class DALMunicipalidad : IDALMunicipalidad
     {
+
+        private NpgsqlConnection iniciarConeccion()
+        {
+            NpgsqlConnection conn = new NpgsqlConnection("Server=127.0.0.1;Port=5432;Database=cerebroDB;User Id=postgres;Password=postgres;");
+            conn.Open();
+            if (conn.State == System.Data.ConnectionState.Open)
+                return conn;
+            return null;
+        }
+
+        public bool agregarMunicipalidad(Municipalidad muni)
+        {
+            if (muni.nombre != null && muni.ubicacion !=null)
+            {
+                NpgsqlConnection conn = iniciarConeccion();
+                NpgsqlCommand command = new NpgsqlCommand("INSERT INTO municipalidades (nombre, ubicacion) VALUES ('" + muni.nombre + "','" + muni.ubicacion + "')", conn);
+                command.ExecuteNonQuery();
+                conn.Close();
+                return true;
+            }
+            return false;
+        }
+
+        public bool borrarMunicipalidad(Municipalidad muni)
+        {
+            if (muni.nombre != null)
+            {
+                NpgsqlConnection conn = iniciarConeccion();
+                NpgsqlCommand command = new NpgsqlCommand("UPDATE municipalidades SET estado=false WHERE nombre='"+ muni.nombre +"'", conn);
+                command.ExecuteNonQuery();
+                conn.Close();
+                return true;
+            }
+            return false;
+        }
+
     }
 }
