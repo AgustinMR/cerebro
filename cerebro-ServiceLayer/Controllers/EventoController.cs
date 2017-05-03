e5b3de5cc7522e6b208970df7556361ee49d93bd
Agustin Maidana <agustin.maidana@fing.edu.uy>
2017-05-03 19:13:30 -0300

    Service Layer parte de mongo

--- cerebro-ServiceLayer/Controllers/EventoController.cs
+++ cerebro-ServiceLayer/Controllers/EventoController.cs
@@ -1,13 +1,53 @@
-﻿using System;
+﻿using cerebro;
+using cerebro_BusinessLogicLayer;
+using MongoDB.Bson;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace cerebro_ServiceLayer.Controllers
 {
+    [RoutePrefix("api/eventos")]
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class EventoController : ApiController
     {
+        [HttpPost]
+        [Route("")]
+        public bool addEvento([FromUri]Evento e)
+        {
+            return new BLEvento().addEvento(e);
+        }
+
+        [HttpDelete]
+        [Route("{id:ObjectId}")]
+        public bool deleteEvento(ObjectId id)
+        {
+            return new BLEvento().deleteEvento(id);
+        }
+
+        [HttpGet]
+        [Route("")]
+        public List<Evento> getAllEventos()
+        {
+            return new BLEvento().getAllEventos();
+        }
+
+        [HttpPost]
+        [Route("{id:ObjectId}")]
+        public Evento getEvento(ObjectId id)
+        {
+            return new BLEvento().getEvento(id);
+        }
+
+        [HttpPut]
+        [Route("")]
+        public bool updateEvento(Evento e)
+        {
+            return new BLEvento().updateEvento(e);
+        }
     }
 }
--- cerebro-ServiceLayer/Controllers/TipoDeFuenteDeDatoController.cs
+++ cerebro-ServiceLayer/Controllers/TipoDeFuenteDeDatoController.cs
@@ -1,13 +1,49 @@
-﻿using System;
+﻿using cerebro;
+using cerebro_BusinessLogicLayer;
+using MongoDB.Bson;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace cerebro_ServiceLayer.Controllers
 {
+    [RoutePrefix("api/tipos")]
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class TipoDeFuenteDeDatoController : ApiController
     {
+        [HttpPost]
+        [Route("")]
+        public bool addTipoDeFuenteDeDato([FromUri]TipoDeFuenteDeDato t)
+        {
+            return new BLTipoDeFuenteDeDato().addTipoDeFuenteDeDato(t);
+        }
+
+        [HttpDelete]
+        [Route("{id:ObjectId}")]
+        public bool deleteTipoDeFuenteDeDato(ObjectId id)
+        {
+            return new BLTipoDeFuenteDeDato().deleteTipoDeFuenteDeDato(id);
+        }
+
+        [HttpGet]
+        [Route("")]
+        public List<TipoDeFuenteDeDato> getAllTipoDeFuenteDeDatos()
+        {
+            return new BLTipoDeFuenteDeDato().getAllTipoDeFuenteDeDatos();
+        }
+
+        [HttpGet]
+        [Route("{id:ObjectId}")]
+        public TipoDeFuenteDeDato getTipoDeFuenteDeDato(ObjectId id)
+        {
+            return new BLTipoDeFuenteDeDato().getTipoDeFuenteDeDato(id);
+        }
+
+        [HttpPut]
+        [Route("")]
+        public bool updateTipoDeFuenteDeDato([FromUri]TipoDeFuenteDeDato t)
+        {
+            return new BLTipoDeFuenteDeDato().updateTipoDeFuenteDeDato(t);
+        }
     }
 }
