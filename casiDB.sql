7f489a60398d5360db395148ad43087d336b70c6
bruno-full-name <bruno.carballido@gmail.com>
2017-05-04 11:49:09 -0300

    DAL sin ODBC y script para sql server2

--- casiDB.sql
+++ casiDB.sql
@@ -1,7 +1,6 @@
 CREATE TABLE MUNICIPALIDADES (
   nombre varchar(30) NOT NULL,
-  ubicacion text NOT NULL,
-  estado BOOLEAN NOT NULL default TRUE,
+  ubicacion varchar(255) NOT NULL,
   PRIMARY KEY (nombre)
 );
 
@@ -9,31 +8,10 @@ CREATE TABLE USUARIOS (
   email varchar(30) NOT NULL,
   nombre_municipalidad varchar(30) NOT NULL,
   nombre varchar(30) NULL,
+  password varchar(30) NULL,
+  tipo integer NOT NULL,
   PRIMARY KEY (email, nombre_municipalidad),
   FOREIGN KEY (nombre_municipalidad) REFERENCES MUNICIPALIDADES (nombre)
-);
-
-CREATE TABLE VISITANTES (
-  visitante_email varchar(30) NOT NULL,
-  nombre_municipalidad varchar(30) NOT NULL,
-  PRIMARY KEY (visitante_email, nombre_municipalidad),
-  FOREIGN KEY (visitante_email, nombre_municipalidad) REFERENCES USUARIOS (email, nombre_municipalidad)
-);
-
-CREATE TABLE OPERADORES (
-  operador_email varchar(30) NOT NULL,
-  password varchar(30) NOT NULL,
-  nombre_municipalidad varchar(30) NOT NULL,
-  PRIMARY KEY (operador_email, nombre_municipalidad),
-  FOREIGN KEY (operador_email, nombre_municipalidad) REFERENCES USUARIOS (email, nombre_municipalidad)
-);
-
-CREATE TABLE ADMINISTRADOR (
-  admin_email varchar(30) NOT NULL,
-  password varchar(30) NOT NULL,
-  nombre_municipalidad varchar(30) NOT NULL,
-  PRIMARY KEY (admin_email, nombre_municipalidad),
-  FOREIGN KEY (admin_email, nombre_municipalidad) REFERENCES USUARIOS (email, nombre_municipalidad)
 );
 
 CREATE TABLE AGRUPACIONES (
@@ -48,8 +26,8 @@ CREATE TABLE AGRUPACIONES_USUARIOS (
   nombre_municipalidad_agrupacion varchar(30) NOT NULL,
   usuario_email varchar(30) NOT NULL,
   nombre_municipalidad_usuario varchar(30) NOT NULL,
-  admin BOOLEAN NOT NULL default FALSE,
+  admin bit NOT NULL,
   PRIMARY KEY (nombre_agrupacion, nombre_municipalidad_agrupacion, usuario_email, nombre_municipalidad_usuario),
-  FOREIGN KEY (nombre_municipalidad_agrupacion, nombre_agrupacion) REFERENCES AGRUPACIONES (nombre_municipalidad, nombre),
-  FOREIGN KEY (nombre_municipalidad_usuario, usuario_email) REFERENCES USUARIOS (nombre_municipalidad, email)
+  FOREIGN KEY (nombre_agrupacion, nombre_municipalidad_agrupacion) REFERENCES AGRUPACIONES (nombre, nombre_municipalidad),
+  FOREIGN KEY (usuario_email, nombre_municipalidad_usuario) REFERENCES USUARIOS (email, nombre_municipalidad)
 );
--- cerebro-DataAccessLayer/DALMunicipalidad.cs
+++ cerebro-DataAccessLayer/DALMunicipalidad.cs
@@ -1,5 +1,4 @@
-﻿using Npgsql;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,39 +10,14 @@ namespace cerebro_DataAccessLayer
     public class DALMunicipalidad : IDALMunicipalidad
     {
 
-        private NpgsqlConnection iniciarConeccion()
-        {
-            NpgsqlConnection conn = new NpgsqlConnection("Server=127.0.0.1;Port=5432;Database=cerebroDB;User Id=postgres;Password=postgres;");
-            conn.Open();
-            if (conn.State == System.Data.ConnectionState.Open)
-                return conn;
-            return null;
-        }
-
         public bool agregarMunicipalidad(Municipalidad muni)
         {
-            if (muni.nombre != null && muni.ubicacion !=null)
-            {
-                NpgsqlConnection conn = iniciarConeccion();
-                NpgsqlCommand command = new NpgsqlCommand("INSERT INTO municipalidades (nombre, ubicacion) VALUES ('" + muni.nombre + "','" + muni.ubicacion + "')", conn);
-                command.ExecuteNonQuery();
-                conn.Close();
-                return true;
-            }
-            return false;
+            return true;
         }
 
         public bool borrarMunicipalidad(Municipalidad muni)
         {
-            if (muni.nombre != null)
-            {
-                NpgsqlConnection conn = iniciarConeccion();
-                NpgsqlCommand command = new NpgsqlCommand("UPDATE municipalidades SET estado=false WHERE nombre='"+ muni.nombre +"'", conn);
-                command.ExecuteNonQuery();
-                conn.Close();
-                return true;
-            }
-            return false;
+            return true;
         }
 
     }
