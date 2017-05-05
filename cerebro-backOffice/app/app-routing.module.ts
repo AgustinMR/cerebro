1ef53580528ee9a1066da54f046ce7b48055dacb
AgustinMR <agustinmr1995@gmail.com>
2017-05-05 17:11:32 -0300

    algunos pocos cambios

---
+++ cerebro-backOffice/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+﻿import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HashLocationStrategy, Location, LocationStrategy } from '@angular/common';
+
+// Fuentes de Dato
+import { FuenteDeDatoComponent } from './dispositivo/dispositivo.component';
+import { FuenteDeDatoService } from './dispositivo/dispositivo.service';
+// Eventos
+import { EventoComponent } from './evento/evento.component'
+import { EventoService } from './evento/evento.service'
+// Tipos de Fuentes de Datos
+import { TipoDeFuenteDeDatoComponent } from './tipo/tipo.component'
+import { TipoDeFuenteDeDatoService } from './tipo/tipo.service'
+// Usuarios
+import { UsuarioComponent } from './usuario/usuario.component'
+import { UsuarioService } from './usuario/usuario.service'
+// Estadistica
+import { EstadisticaComponent } from './estadistica/estadistica.component'
+import { EstadisticaService } from './estadistica/estadistica.service'
+
+const routes: Routes = [
+    { path: 'dispositivos', component: FuenteDeDatoComponent },
+    { path: 'estadisticas', component: EstadisticaComponent },
+    { path: 'eventos', component: EventoComponent },
+    { path: 'tipos', component: TipoDeFuenteDeDatoComponent },
+    { path: 'usuarios', component: UsuarioComponent },
+    { path: '', redirectTo: 'estadisticas', pathMatch: 'full' }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+    providers: [Location, { provide: LocationStrategy, useClass: HashLocationStrategy }, FuenteDeDatoService, EstadisticaService, EventoService, TipoDeFuenteDeDatoService, UsuarioService]
+})
+export class AppRoutingModule { }
---
+++ cerebro-backOffice/app/evento/evento.component.ts
@@ -0,0 +1,31 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { EventoService } from './evento.service';
+import { Http, HttpModule, Response } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+
+@Component({
+    selector: 'cerebro-evento',
+    templateUrl: 'evento.component.html',
+    styleUrls: ['evento.component.css'],
+    moduleId: module.id,
+    providers: [EventoService]
+})
+export class EventoComponent implements OnInit {
+
+    employees: any;
+
+    constructor(private eventos: EventoService) {
+    }
+
+    ngOnInit(): void {
+        //this.getEmployees();
+    }
+    /*
+    getEmployees() {
+        this.employeeService.getEmployees().subscribe(
+            (data: Response) => this.employees = data,
+            responseError => console.log(responseError),
+            () => console.log("Employee Fetching operation completed")
+        );
+    }*/
+}
