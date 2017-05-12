736c07efa576f12ecbccf6dd215d1dfd83ffabb9
bruno-full-name <bruno.carballido@gmail.com>
2017-05-12 19:20:35 -0300

    Revert "chau"
    This reverts commit 404b09cda69774f9c35440601c2de034680afb9f.

---
+++ cerebro-ServiceLayer/Controllers/FuenteDeDatoController.cs
@@ -0,0 +1,53 @@
+﻿using cerebro;
+using cerebro_BusinessLogicLayer;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace cerebro_ServiceLayer.Controllers
+{
+    [RoutePrefix("api/dispositivos")]
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class FuenteDeDatoController : ApiController
+    {
+        [HttpPost]
+        [Route("")]
+        public bool addFuenteDeDato([FromUri]FuenteDeDato f)
+        {
+            return new BLFuenteDeDato().addFuenteDeDato(f);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public bool deleteFuenteDeDato(string id)
+        {
+            return new BLFuenteDeDato().deleteFuenteDeDato(ObjectId.Parse(id));
+        }
+
+        [HttpGet]
+        [Route("")]
+        public List<FuenteDeDato> getAllFuenteDeDato()
+        {
+            return new BLFuenteDeDato().getAllFuenteDeDato();
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public FuenteDeDato getFuenteDeDato(string id)
+        {
+            return new BLFuenteDeDato().getFuenteDeDato(ObjectId.Parse(id));
+        }
+
+        [HttpPut]
+        [Route("")]
+        public bool updateFuenteDeDato([FromUri]FuenteDeDato f)
+        {
+            return new BLFuenteDeDato().updateFuenteDeDato(f);
+        }
+    }
+}
