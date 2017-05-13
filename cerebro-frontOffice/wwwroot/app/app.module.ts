0aa842f108346d6d90dd1d4b694e4e9184da740d
AgustinMR <agustinmr1995@gmail.com>
2017-05-13 17:39:32 -0300

    skjfnfdskjgvndfkjgh

--- cerebro-frontOffice/wwwroot/app/app.module.ts
+++ cerebro-frontOffice/wwwroot/app/app.module.ts
@@ -1,15 +1,24 @@
 import { NgModule, enableProdMode } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpModule, JsonpModule } from '@angular/http';
 import { BrowserModule, Title } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { routing, routedComponents } from './app.routing';
 import { APP_BASE_HREF, Location } from '@angular/common';
 import { AppComponent } from './app.component';
+import "rxjs/add/operator/map";
+
+import { AgrupacionService } from './agrupacion.service';
+import { ChatService } from './chat.service';
+import { IndexService } from './index.service';
 
 // enableProdMode();
 
 @NgModule({
-    imports: [BrowserModule, routing],
+    imports: [BrowserModule, routing, HttpModule, JsonpModule, CommonModule, FormsModule, ReactiveFormsModule],
     declarations: [AppComponent, routedComponents],
-    providers: [Title, { provide: APP_BASE_HREF, useValue: '/' }],
+    providers: [Title, { provide: APP_BASE_HREF, useValue: '/' }, AgrupacionService, ChatService, IndexService],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
