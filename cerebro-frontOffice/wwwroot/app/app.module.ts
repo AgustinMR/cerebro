1b5feaf3968fdfdf3edc2ce25dd4ceaa163435fa
AgustinMR <agustinmr1995@gmail.com>
2017-05-19 16:13:39 -0300

    did you see the falling bombs?

--- cerebro-frontOffice/wwwroot/app/app.module.ts
+++ cerebro-frontOffice/wwwroot/app/app.module.ts
@@ -10,15 +10,19 @@ import { AppComponent } from './app.component';
 import "rxjs/add/operator/map";
 
 import { AgrupacionService } from './agrupacion.service';
-import { ChatService } from './chat.service';
+import { ChatService, ChannelConfig, SignalrWindow } from './chat.service';
 import { IndexService } from './index.service';
+
+let channelConfig = new ChannelConfig();
+channelConfig.url = "http://localhost:9123/signalr";
+channelConfig.hubName = "ChatSignalrHub";
 
 // enableProdMode();
 
 @NgModule({
     imports: [BrowserModule, routing, HttpModule, JsonpModule, CommonModule, FormsModule, ReactiveFormsModule],
     declarations: [AppComponent, routedComponents],
-    providers: [Title, { provide: APP_BASE_HREF, useValue: '/' }, AgrupacionService, ChatService, IndexService],
+    providers: [Title, { provide: APP_BASE_HREF, useValue: '/' }, AgrupacionService, ChatService, IndexService, { provide: SignalrWindow, useValue: window }, { provide: 'channel.config', useValue: channelConfig }],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
--- cerebro-frontOffice/wwwroot/app/chat.component.ts
+++ cerebro-frontOffice/wwwroot/app/chat.component.ts
@@ -1,9 +1,71 @@
-﻿import { Component } from '@angular/core';
+﻿import { OnInit, Input, Component } from "@angular/core";
+import { Http, HttpModule, Response } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+import { ChatService, ChannelEvent } from "./chat.service";
 
 @Component({
     selector: 'my-contact',
-    templateUrl: '/partial/ChatComponent'
+    templateUrl: '/partial/ChatComponent',
+    providers: [ChatService]
 })
 
-export class ChatComponent {
+export class ChatComponent implements OnInit {
+
+    @Input() apiUrl: string;
+    @Input() channel: string;
+    eventName = "user.connected";
+
+    messages = "";
+
+    constructor(private http: Http, private channelService: ChatService) {
+
+    }
+
+    ngOnInit() {
+        // Get an observable for events emitted on this channel
+        //
+        /*this.channelService.sub(this.channel).subscribe(
+            (x: ChannelEvent) => {
+                if (x.Name === "user.subscribed") {
+                    alert();
+                    this.appendStatusUpdate(x);
+                }
+            },
+            (error: any) => {
+                console.warn("Attempt to join channel failed!", error);
+            }
+        );*/
+        this.channelService.sub(this.channel).map(response => {
+        }).subscribe(response => console.log("Message recibed on EmployeeTaskComponent ngOnInit method"), error => console.log("Ha ocurrido un error: ", error), () => { });
+    }
+
+
+    private appendStatusUpdate(ev: ChannelEvent): void {
+        // Just prepend this to the messages string shown in the textarea
+        //
+        alert();
+        let date = new Date();
+        switch (ev.data.State) {
+            case "starting": {
+                this.messages = `${date.toLocaleTimeString()} : starting\n` + this.messages;
+                break;
+            }
+
+            case "complete": {
+                this.messages = `${date.toLocaleTimeString()} : complete\n` + this.messages;
+                break;
+            }
+
+            default: {
+                this.messages = `${date.toLocaleTimeString()} : ${ev.data.State} : ${ev.data.PercentComplete} % complete\n` + this.messages;
+            }
+        }
+    }
+
+    callApi() {
+        this.http.get(this.apiUrl)
+            .map((res: Response) => res.json())
+            .subscribe((message: string) => { console.log(message); });
+    }
+
 }
