db816a8f89d6d1e9da1358d5dc507fce374f0a48
AgustinMR <agustinmr1995@gmail.com>
2017-05-19 16:14:07 -0300

    whatever

--- cerebro-frontOffice/wwwroot/app/chat.component.js
+++ cerebro-frontOffice/wwwroot/app/chat.component.js
@@ -5,17 +5,78 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 var core_1 = require("@angular/core");
+var http_1 = require("@angular/http");
+require("rxjs/add/operator/toPromise");
+var chat_service_1 = require("./chat.service");
 var ChatComponent = (function () {
-    function ChatComponent() {
+    function ChatComponent(http, channelService) {
+        this.http = http;
+        this.channelService = channelService;
+        this.eventName = "user.connected";
+        this.messages = "";
     }
+    ChatComponent.prototype.ngOnInit = function () {
+        // Get an observable for events emitted on this channel
+        //
+        /*this.channelService.sub(this.channel).subscribe(
+            (x: ChannelEvent) => {
+                if (x.Name === "user.subscribed") {
+                    alert();
+                    this.appendStatusUpdate(x);
+                }
+            },
+            (error: any) => {
+                console.warn("Attempt to join channel failed!", error);
+            }
+        );*/
+        this.channelService.sub(this.channel).map(function (response) {
+        }).subscribe(function (response) { return console.log("Message recibed on EmployeeTaskComponent ngOnInit method"); }, function (error) { return console.log("Ha ocurrido un error: ", error); }, function () { });
+    };
+    ChatComponent.prototype.appendStatusUpdate = function (ev) {
+        // Just prepend this to the messages string shown in the textarea
+        //
+        alert();
+        var date = new Date();
+        switch (ev.data.State) {
+            case "starting": {
+                this.messages = date.toLocaleTimeString() + " : starting\n" + this.messages;
+                break;
+            }
+            case "complete": {
+                this.messages = date.toLocaleTimeString() + " : complete\n" + this.messages;
+                break;
+            }
+            default: {
+                this.messages = date.toLocaleTimeString() + " : " + ev.data.State + " : " + ev.data.PercentComplete + " % complete\n" + this.messages;
+            }
+        }
+    };
+    ChatComponent.prototype.callApi = function () {
+        this.http.get(this.apiUrl)
+            .map(function (res) { return res.json(); })
+            .subscribe(function (message) { console.log(message); });
+    };
     return ChatComponent;
 }());
+__decorate([
+    core_1.Input(),
+    __metadata("design:type", String)
+], ChatComponent.prototype, "apiUrl", void 0);
+__decorate([
+    core_1.Input(),
+    __metadata("design:type", String)
+], ChatComponent.prototype, "channel", void 0);
 ChatComponent = __decorate([
     core_1.Component({
         selector: 'my-contact',
-        templateUrl: '/partial/ChatComponent'
-    })
+        templateUrl: '/partial/ChatComponent',
+        providers: [chat_service_1.ChatService]
+    }),
+    __metadata("design:paramtypes", [http_1.Http, chat_service_1.ChatService])
 ], ChatComponent);
 exports.ChatComponent = ChatComponent;
 //# sourceMappingURL=chat.component.js.map
