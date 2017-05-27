f27f073ce230ca9ed688a9672516655113861267
Pedro Omar Moretto Rodriguez <pedro.moretto@fing.edu.uy>
2017-05-27 00:55:49 -0300

    cosas de pedros

--- cerebro-frontOffice/obj/Release/netcoreapp1.1/PubTmp/Out/wwwroot/app/app.routing.js
+++ cerebro-frontOffice/obj/Release/netcoreapp1.1/PubTmp/Out/wwwroot/app/app.routing.js
@@ -1,5 +1,4 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 var router_1 = require("@angular/router");
 var agrupacion_component_1 = require("./agrupacion.component");
 var chat_component_1 = require("./chat.component");
