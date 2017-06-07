b043b35a586809f167c5575d560f485a4f6dd253
bruno-full-name <bruno.carballido@gmail.com>
2017-06-07 12:58:50 -0300

    login

---
+++ cerebro-frontOffice/Models/Municipalidades.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cerebro_frontOffice
+{
+    public partial class Municipalidades
+    {
+        public Municipalidades()
+        {
+            Agrupaciones = new HashSet<Agrupaciones>();
+            Privilegios = new HashSet<Privilegios>();
+            Usuarios = new HashSet<Usuarios>();
+        }
+
+        public string Nombre { get; set; }
+        public string Ubicacion { get; set; }
+
+        public virtual ICollection<Agrupaciones> Agrupaciones { get; set; }
+        public virtual ICollection<Privilegios> Privilegios { get; set; }
+        public virtual ICollection<Usuarios> Usuarios { get; set; }
+    }
+}
---
+++ cerebro-frontOffice/Models/Usuarios.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cerebro_frontOffice
+{
+    public partial class Usuarios
+    {
+        public Usuarios()
+        {
+            AgrupacionesUsuarios = new HashSet<AgrupacionesUsuarios>();
+            PrivilegiosUsuarios = new HashSet<PrivilegiosUsuarios>();
+        }
+
+        public string Email { get; set; }
+        public string NombreMunicipalidad { get; set; }
+        public string Nombre { get; set; }
+        public string Password { get; set; }
+        public int Tipo { get; set; }
+        public bool Enabled { get; set; }
+
+        public virtual ICollection<AgrupacionesUsuarios> AgrupacionesUsuarios { get; set; }
+        public virtual ICollection<PrivilegiosUsuarios> PrivilegiosUsuarios { get; set; }
+        public virtual Municipalidades NombreMunicipalidadNavigation { get; set; }
+    }
+}
