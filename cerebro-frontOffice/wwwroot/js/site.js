0b286f00b911f14aa2fa07a3c155ed7c0d32698d
bruno-full-name <bruno.carballido@gmail.com>
2017-06-26 19:37:10 -0300

    envio de video

--- cerebro-frontOffice/wwwroot/js/site.js
+++ cerebro-frontOffice/wwwroot/js/site.js
@@ -83,6 +83,25 @@ function sendTxt(id, frec, web, nombre) {
     }
 }
 
+function sendVid(id, frec, web, nombre) {
+    for (i = 0; i < sim.length; i++) {
+        if (sim[i] === id) {
+            setTimeout(function () {
+                try {
+                    var medida = document.getElementById("video" + id).value;
+                    $.post("https://www.cerebro-frontOffice.com/api/dispositivos?dispositivoId=" + id + "&nombre=" + nombre + "&tipoDeDato=Video&medida=" + medida + "&imagenId=", function (res) {
+                        console.log(res);
+                    });
+                    sendTxt(id, frec, web, nombre);
+                }
+                catch (err) {
+                    console.log("Div borrado");
+                }
+            }, frec * 1000);
+        }
+    }
+}
+
 function sendImg(id, frec, web, nombre) {
     for (i = 0; i < sim.length; i++) {
         if (sim[i] === id) {
@@ -521,6 +540,114 @@ function newDispositivoImagen(dispositivo, tipo, x) {
                 segments.appendChild(segment2);
                 segments.appendChild(segment3);
                 document.getElementById("main").insertBefore(segments, document.getElementById("main").firstChild);
+            }
+        });
+    }
+}
+
+function newDispositivoVideo(dispositivo) {
+    "use strict";
+    var bool = true;
+    for (var i = 0; i < sim.length; i++) {
+        if (sim[i] === dispositivo.Id) {
+            bool = false;
+        }
+    }
+
+    if (bool) {
+        sim[sim.length] = dispositivo.Id;
+        $.get("https://www.cerebro-serviceLayer.com/api/tipos/" + dispositivo.tipo, function (res) {
+            if (res !== null) {
+                var frec = res.frecuenciaLectura;
+                var web = res.uriWebService;
+                var nombre = dispositivo.nombre;
+
+                var segments = document.createElement("div");
+                segments.className = "ui segments";
+                segments.id = "dispositivo" + dispositivo.Id;
+                var segment1 = document.createElement("div");
+                segment1.className = "ui secondary grey segment";
+                var segment2 = document.createElement("div");
+                segment2.className = "ui segment";
+                var segment3 = document.createElement("div");
+                segment3.className = "ui right aligned segment";
+                // ****************** segment 1 **********************//
+                var h4_1 = document.createElement("h4");
+                h4_1.className = "ui left aligned header w3-left";
+                h4_1.style.marginTop = "-10px";
+                h4_1.innerHTML = dispositivo.municipalidad;
+                var h4_2 = document.createElement("h4");
+                h4_2.className = "ui left aligned header w3-left w3-text-cerebro-red";
+                h4_2.style.marginTop = "-10px";
+                h4_2.style.marginLeft = "7px";
+                h4_2.innerHTML = dispositivo.nombre;
+                var button = document.createElement("button");
+                button.className = "ui icon button very small compact w3-right";
+                button.style.marginTop = "-14px";
+                button.style.marginRight = "-14px";
+                button.addEventListener("click", function () {
+                    for (var h = 0; h < sim.length; h++) {
+                        if (sim[h] === dispositivo.Id) {
+                            sim[h] = 0;
+                        }
+                    }
+                    for (var f = 0; f < env.length; f++) {
+                        if (env[f] === dispositivo.Id) {
+                            env[f] = 0;
+                        }
+                    }
+                    document.getElementById("main").removeChild(segments);
+                });
+                var i1 = document.createElement("i");
+                i1.className = "remove icon w3-text-cerebro-red";
+                button.appendChild(i1);
+                segment1.appendChild(h4_1);
+                segment1.appendChild(h4_2);
+                segment1.appendChild(button);
+                // ****************** segment 2 **********************//
+                var h4_3 = document.createElement("h4");
+                h4_3.className = "ui horizontal divider text header";
+                h4_3.innerHTML = "Datos a enviar";
+                var divInput = document.createElement("div");
+                divInput.className = "ui labeled input fluid w3-margin-top";
+                var label3 = document.createElement("div");
+                label3.className = "ui label";
+                label3.innerHTML = "Video:";
+                var input3 = document.createElement("input");
+                input3.type = "text";
+                input3.id = "video" + dispositivo.Id;
+                input3.placeholder = "url del video a ser enviado por el dispositivo simulado...";
+                divInput.appendChild(label3);
+                divInput.appendChild(input3);
+                segment2.appendChild(divInput);
+                //****************** segment 3 **********************//
+                var divPlay = document.createElement("div");
+                divPlay.className = "ui inverted icon buttons";
+                var play = document.createElement("button");
+                play.className = "ui button";
+                play.addEventListener("click", function () {
+                    var t = true;
+                    for (var j = 0; j < env.length; j++) {
+                        if (env[j] === dispositivo.Id) {
+                            t = false;
+                        }
+                    }
+                    if (t) {
+                        env[env.length] = dispositivo.Id;
+                        sendVid(dispositivo.Id, frec, web, nombre);
+                    }
+                });
+                var iPlay = document.createElement("i");
+                iPlay.className = "play green icon";
+                play.appendChild(iPlay);
+                divPlay.appendChild(play);
+                segment3.appendChild(divPlay);
+
+                segments.appendChild(segment1);
+                segments.appendChild(segment2);
+                segments.appendChild(segment3);
+                document.getElementById("main").insertBefore(segments, document.getElementById("main").firstChild);
+                $('.ui.dropdown').dropdown('setting', 'allowAdditions', true);
             }
         });
     }
